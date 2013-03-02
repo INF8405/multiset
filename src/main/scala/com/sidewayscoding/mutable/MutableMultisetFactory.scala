71e6818f8c86b7a87bb4d96b20d1de5c021c78a9
Jean-Remi Desjardins <jedesah@localhost.localdomain>
2013-03-02 16:45:26 -0500

    Thrashing all the mutable stuff and most of the tests to get it quickly working with Scala 2.10.0

--- src/main/scala/com/sidewayscoding/mutable/MutableMultisetFactory.scala
+++
@@ -1,6 +0,0 @@
-package com.sidewayscoding.mutable
-
-import com.sidewayscoding.MultisetFactory
-
-abstract class MutableMultisetFactory[CC[X] <: com.sidewayscoding.mutable.Multiset[X] with MultisetLike[X, CC[X]]] 
-         extends MultisetFactory[CC]
